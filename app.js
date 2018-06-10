217a966e119d74a089548fbb2f70ee5b05cc3975
Ibrahim Aksoy <ib.koray@gmail.com>
2018-06-10 20:01:51 +0300

    Updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-var cat = document.getElementById('cat');
-var count = document.getElementById('count');
-var clickCount = 0;
+var firstCount = document.getElementById('first').getElementsByClassName('count');
+var secondCount = document.getElementById('second').getElementsByClassName('count');
+var firstClickCount = 0;
+var secondClickCount = 0;
 
-cat.addEventListener('click', function(){
-  clickCount++;
-  count.innerHTML = clickCount;
-}, false);
+document.getElementById('poplinre').addEventListener('click', function () {
+  	firstClickCount++;
+  	console.log(firstClickCount);
+  	firstCount[0].innerHTML = firstClickCount;
+  	console.log(firstCount);
+});
+
+document.getElementById('chewie').addEventListener('click', function () {
+  	secondClickCount++;
+  	console.log(secondClickCount);
+  	secondCount[0].innerHTML = secondClickCount;
+  	console.log(secondCount);
+});
