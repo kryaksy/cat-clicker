d6cd50470adc1671c261da554d99029546dc5654
Ibrahim Aksoy <ib.koray@gmail.com>
2018-06-15 19:27:34 +0300

    Updated app.js

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
-var firstCount = document.getElementById('first').getElementsByClassName('count');
-var secondCount = document.getElementById('second').getElementsByClassName('count');
-var firstClickCount = 0;
-var secondClickCount = 0;
+// Selecting Elements
+var list = document.getElementById('list'),
+	ul = list.getElementsByTagName('ul'),
+	li = list.getElementsByTagName('li'),
+	container = document.getElementById('container'),
+	count = document.getElementById('count'),
+	image = container.getElementsByTagName('img');
 
-document.getElementById('poplinre').addEventListener('click', function () {
-  	firstClickCount++;
-  	console.log(firstClickCount);
-  	firstCount[0].innerHTML = firstClickCount;
-  	console.log(firstCount);
-});
+// Defining Count Array
+var counts = [];
+for (var i = 0; i < li.length; i++) {
+	counts[i] = 0;
+}
 
-document.getElementById('chewie').addEventListener('click', function () {
-  	secondClickCount++;
-  	console.log(secondClickCount);
-  	secondCount[0].innerHTML = secondClickCount;
-  	console.log(secondCount);
-});
+(function init() {
+	image[0].src = li[0].innerHTML + '.jpg';
+	image[0].class = li[0].innerHTML;
+	count.innerHTML = counts[0];
+}());
+
+for (var i = 0; i < li.length; i++) {
+	li[i].addEventListener('click', (function (iCopy) {
+		return function () {
+			image[0].src = li[iCopy].innerHTML + '.jpg';
+			image[0].class = li[iCopy].innerHTML;
+			count.innerHTML = counts[iCopy];
+		}
+	})(i));
+};
+
+for (var i = 0; i < li.length; i++) {
+	image[0].addEventListener('click', (function (iCopy) {
+		return function () {
+			if (image[0].class === li[iCopy].innerHTML) {
+				counts[iCopy] += 1;
+				count.innerHTML = counts[iCopy];
+			}
+		}
+	})(i));
+};
